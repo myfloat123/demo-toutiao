76afae9f947c1d0e1d5a3777770712e7c977930d
xyf <2767252511@qq.com>
2022-11-08 13:33:50 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入并安装 vant 组件库
+// 1. 引入你需要的组件
+import Vant from 'vant'
+// 2. 引入组件样式
+// 切记：为了能够覆盖默认的 less 变量，这里一定要把后缀名改为 .less
+import 'vant/lib/index.less'
+// 3. 注册你需要的组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,35 @@
+// 这个文件是 vue-cli 创建出来的项目的配置文件
+// 在 vue.config.js 这个配置文件中，可以对整个项目的打包、构建进行全局性的配置
+
+// webpack 在进行打包的时候，底层用到了 node.js
+// 因此，在 vue.config.js 配置文件中，可以导入并使用 node.js 中的核心模块
+const path = require('path')
+const themePath = path.join(__dirname, './src/theme.less')
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false
 })
+
+// vue.config.js
+module.exports = {
+  publicPath: './',
+  css: {
+    loaderOptions: {
+      less: {
+        // 若 less-loader 版本小于 6.0，请移除 lessOptions 这一级，直接配置选项。
+        lessOptions: {
+          modifyVars: {
+            // 直接覆盖变量
+            // 'nav-bar-background-color': 'red',
+            'text-color': '#111',
+            // 'border-color': '#eee',
+            // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+            hack: `true; @import "${themePath}";`
+          }
+        }
+      }
+    }
+  }
+}
